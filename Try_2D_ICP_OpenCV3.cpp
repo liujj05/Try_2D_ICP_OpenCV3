5d140ae233efd1175d327439e2c239f4ebea7eda
liujj05 <liujj05@users.noreply.github.com>
2018-07-12 21:10:27 +0800

    输出最终结果
    添加将阶段结果整合的步骤
    
    以R_res，t_res为最终结果进行输出
    
    同时定义了一个宏命令PRINT_OUT决定是否输出所有中间结果

--- Try_2D_ICP_OpenCV3.cpp
+++ Try_2D_ICP_OpenCV3.cpp
@@ -19,6 +19,8 @@ using namespace cv;
 #define ITER_MAX 100
 #define ITER_THRESH 0.001
 
+// 是否输出数据
+//#define PRINT_OUT
 
 int main()
 {
@@ -167,9 +169,9 @@ int main()
 	// 2.1 准备
 	float pre_err = FLT_MAX;	// 迭代需要的误差量存储1
 	float now_err = 0;			// 迭代需要的误差量存储2
-	float delta_err;
-	Mat res_ind, res_dist;
-	int res_ind_int = 0;
+	float delta_err;			// 收敛判定依据
+	Mat res_ind, res_dist;		// K-D Tree 查找结果返回值
+	int res_ind_int = 0;		// 查找索引提取
 	float near_x, near_y;
 	Mat nearest_pts_from_ref = Mat(ICP_new_pts.rows, 2, CV_32FC1);
 	Mat mean_new = Mat(1, 2, CV_32FC1);		// ICP 计算过程中求解各个步骤的R、t的中间量，待匹配点云的重心
@@ -178,9 +180,14 @@ int main()
 	Mat H, U, S, Vt; // 中间量
 	Mat Mid_eye = Mat::eye(2, 2, CV_32FC1);
 	Mat temp_new_pts;
-	Mat R, t; // 计算结果
+	Mat R, t; // 阶段计算结果
+	Mat R_res, t_res; // 汇总的最终结果
+	R_res = Mat::eye(2, 2, CV_32FC1);
+	t_res = Mat::zeros(2, 1, CV_32FC1);
 
-	
+	// 为了保留每个new_pts，复制矩阵 ICP_new_pts 至 ICP_new_pts_origin
+	Mat ICP_new_pts_origin;
+	ICP_new_pts.copyTo(ICP_new_pts_origin);
 
 
 	// 2.2 迭代开始
@@ -202,8 +209,10 @@ int main()
 									  (ICP_new_pts.at<float>(i, 1) - near_y) * (ICP_new_pts.at<float>(i, 1) - near_y));
 		}
 
+#ifdef PRINT_OUT
 		cout << "nearest: " << endl;
 		cout << nearest_pts_from_ref << endl;
+#endif
 
 		delta_err = abs(now_err - pre_err);
 
@@ -220,6 +229,7 @@ int main()
 		mean_near.at<float>(0, 0) = mean(nearest_pts_from_ref.col(0))[0];
 		mean_near.at<float>(0, 1) = mean(nearest_pts_from_ref.col(1))[0];
 
+#ifdef PRINT_OUT
 
 		cout << "mean_new:" << endl;
 		cout << mean_new << endl;
@@ -227,10 +237,13 @@ int main()
 		cout << "mean_near" << endl;
 		cout << mean_near << endl;
 
+#endif
 
 		// 所有点按重心归一化
 		AXY = ICP_new_pts - repeat(mean_new, ICP_new_pts.rows, 1);
 		BXY = nearest_pts_from_ref - repeat(mean_near, nearest_pts_from_ref.rows, 1);
+
+#ifdef PRINT_OUT
 
 		cout << "AXY:" << endl;
 		cout << AXY << endl;
@@ -238,12 +251,13 @@ int main()
 		cout << "BXY:" << endl;
 		cout << BXY << endl;
 
-
+#endif
 
 		// 求出待SVD分解的H矩阵
 		H = AXY.t() * BXY;
 		SVD::compute(H, S, U, Vt);
 
+#ifdef PRINT_OUT
 
 		cout << "H:" << endl;
 		cout << H << endl;
@@ -254,10 +268,13 @@ int main()
 		cout << "Vt" << endl;
 		cout << Vt << endl;
 
+#endif
 
 		Mid_eye.at<float>(1, 1) = determinant(Vt.t()*U.t());
 		R = Vt.t() * Mid_eye * U.t();
 		t = mean_near.t() - R * mean_new.t();
+
+#ifdef PRINT_OUT
 
 		cout << "R:" << endl;
 		cout << R << endl;
@@ -265,19 +282,28 @@ int main()
 		cout << "t" << endl;
 		cout << t << endl;
 
+#endif
+
 		transpose((R * ICP_new_pts.t() + repeat(t, 1, ICP_new_pts.rows)), temp_new_pts);
 		temp_new_pts.copyTo(ICP_new_pts);
+
+#ifdef PRINT_OUT
 
 		cout << "ICP_new_pts: " << endl;
 		cout << ICP_new_pts << endl;
 
+#endif
+		R_res = R * R_res;
+		t_res = R * t_res + t;
 	}
 
-	cout << "R:" << endl;
-	cout << R << endl;
 
-	cout << "t" << endl;
-	cout << t << endl;
+	
+	cout << "R_res:" << endl;
+	cout << R_res << endl;
+
+	cout << "t_res" << endl;
+	cout << t_res << endl;
 
 	cout << "now_err" << endl;
 	cout << now_err << endl;
